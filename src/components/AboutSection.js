c4afe99eba4a656b5bae1aec212071f16d9c3e73
Carl Davidson <carl.davidson@hotmail.com>
2020-12-04 09:52:53 +1100

    add styling to about Section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,30 +1,66 @@
 import React from 'react'
 import home1 from '../img/home1.png'
+import styled from 'styled-components'
 
 const AboutSection = () => {
     return (
-        <div>
-            <div className="description"></div>
-            <section className="title">
-                <div className="hide">
-                    <h2>We Work to Make</h2>
-                </div>
-                <div className="hide">
-                    <h2>
-                        Your <span>dreams</span> come
-                    </h2>
-                </div>
-                <div className="hide">
-                    <h2>true</h2>
-                </div>
-            </section>
-            <p>Contact Us for stuff</p>
-            <button>Contact Us</button>
-            <div className="image">
+        <About>
+            <Description>
+                <section className="title">
+                    <Hide>
+                        <h2>We Work to Make</h2>
+                    </Hide>
+                    <Hide>
+                        <h2>
+                            Your <span>dreams</span> come
+                        </h2>
+                    </Hide>
+                    <Hide>
+                        <h2>true</h2>
+                    </Hide>
+                </section>
+                <p>Contact Us for stuff</p>
+                <button>Contact Us</button>
+            </Description>
+            <Image className="image">
                 <img src={home1} alt="Guy with a camera" />
-            </div>
-        </div>
+            </Image>
+        </About>
     )
 }
+
+//styled components
+const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`
+
+const Description = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+
+    h2 {
+        font-weight: lighter;
+    }
+`
+
+const Image = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img {
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`
+
+const Hide = styled.div`
+overflow: hidden;
+
+`
 
 export default AboutSection
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -3,10 +3,26 @@ import FaqCard from './FaqCard'
 
 const FaqSection = () => {
     const faq = [
-        { question: 'How do I start? 1', answer: 'Lorem, ipsum dolor. 1' },
-        { question: 'Daily Schedule. 2', answer: 'Lorem, ipsum dolor. 2' },
-        { question: 'Payment Methods. 3', answer: 'Lorem, ipsum dolor. 3' },
-        { question: 'What products do you offer. 4', answer: 'Lorem, ipsum dolor. 4' },
+        {
+            id: 1,
+            question: 'How do I start? 1',
+            answer: 'Lorem, ipsum dolor. 1',
+        },
+        {
+            id: 2,
+            question: 'Daily Schedule. 2',
+            answer: 'Lorem, ipsum dolor. 2',
+        },
+        {
+            id: 3,
+            question: 'Payment Methods. 3',
+            answer: 'Lorem, ipsum dolor. 3',
+        },
+        {
+            id: 4,
+            question: 'What products do you offer. 4',
+            answer: 'Lorem, ipsum dolor. 4',
+        },
     ]
 
     return (
@@ -15,7 +31,11 @@ const FaqSection = () => {
                 Any Questions? <span>FAQ</span>{' '}
             </h2>
             {faq.map((q) => (
-                <FaqCard question={q.question} answer={q.answer} />
+                <FaqCard
+                    key={q.id.toString()}
+                    question={q.question}
+                    answer={q.answer}
+                />
             ))}
         </div>
     )
